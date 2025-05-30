9155ba0ae7d2dac70f9d902e6e2581a9f3234c8e
Ayushman <aayushmaang13@gmail.com>
2025-05-30 22:24:38 +0530

    Implement Home Page UI

---
+++ components/CTA.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const CTA = () => {
+    return (
+        <section className="cta-section">
+            <div className="cta-badge">Start learning your way.</div>
+            <h2 className="text-2xl font-bold">
+                Build a Personalized Learning Companion
+            </h2>
+            <p>Pick a name, subject, voice, & personality - and start learning through voice conversations that feel natural fun.</p>
+            <Image src="/images/cta.svg" alt="cta" width={362} height={232} />
+            <button className="btn-primary">
+                <Image src="/icons/plus.svg" alt="plus" width={12} height={12} className="md:hidden" />
+                <Link href="/companions/new">
+                    <p>Build a New Companion</p>
+                </Link>
+            </button>
+        </section>
+    );
+};
+
+export default CTA;
