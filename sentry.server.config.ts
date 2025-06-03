09c2cc269d0d8bec8a583d6f1ad6aa8382625d9a
Ayushman <aayushmaang13@gmail.com>
2025-06-04 00:33:36 +0530

    Implement Sentry error tracking and Session History

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://90e4a1edcebf60ea1005e0705b48fac4@o4509436162605056.ingest.de.sentry.io/4509436178989136",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
